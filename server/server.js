0dbdcfeaaf5314d2d878988a1c87db086a094d17
p-porciello <pporciello@ucvts.org>
2026-01-10 16:55:49 -0500

    Debugged connection with MongoDB

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 // imports
 const express = require("express");
 const app = express();
+const connect = require("./mongoConnect");
 const cors = require("cors");
 const corsOptions = {
     origin: ["http://localhost:5173"],
@@ -21,12 +22,13 @@ app.use('/user', userRouter);
 // console.log(process.env.DATABASE_URI); --> for debugging
 
 //mongodb configuration (this took forever holy-)
+/*
 const mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE_URI);
 const db = mongoose.connection;
 db.on('error', error => console.error(error));
 db.once('open', () => console.log('Connected to Mongoose'));
-
+*/
 app.get("/api", (req, res) =>{
     res.json({lostInfo:["lost items", "poster"]});
 });
